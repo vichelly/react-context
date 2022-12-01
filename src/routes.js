baf127b11659bfb177192c7d2ded5bc30a99c0fe
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-12-01 15:22:45 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import Feira from 'pages/Feira';
+import Login from 'pages/Login';
+import Carrinho from 'pages/Carrinho';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { UsuarioProvider } from 'common/context/Usuario';
+import { CarrinhoProvider } from 'common/context/Carrinho';
+import { PagamentoProvider } from 'common/context/Pagamento';
+
+export default function Router() {
+
+    return(
+        <BrowserRouter>
+            <Switch>
+                <UsuarioProvider>
+                    <Route exact path="/" >    
+                        <Login />
+                    </Route>
+                    <CarrinhoProvider>
+                        <Route path='/feira' >
+                            <Feira />
+                        </Route>
+                        <PagamentoProvider>
+                            <Route path='/carrinho' >
+                                <Carrinho />
+                            </Route>
+                        </PagamentoProvider>
+                    </CarrinhoProvider>
+                </UsuarioProvider>
+            </Switch>
+        </BrowserRouter>
+    )
+}
